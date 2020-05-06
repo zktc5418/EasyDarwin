dd41127c60cadc1f4c602680db859b185be01b61
秦治华 <826619176@qq.com>
2020-05-06 18:07:22 +0800

    修改openfile

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 CWD=$(cd "$(dirname $0)";pwd)
 "$CWD"/easydarwin install
-"$CWD"/easydarwin start 
+"$CWD"/easydarwin start
+PID=$(ps -ef | grep "easydarwin" | grep -v grep | awk '{print $2}')
+prlimit --pid ${PID} --nofile=40000:40000
